361913ecd7469adf653d4d021c7779ce2fe8ce00
Jeremy Ward <exochos@gmail.com>
2025-01-18 14:22:46 -0800

    Update page.tsx

--- app/estimates/page.tsx
+++ app/estimates/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 import Link from "next/link";
 
 export default function Home() {
-  const heroText = "Get in Touch for Estimates";
+  const heroText = "Request an Estimate";
   const heroImage = "/images/familys-historic-home.jpg";
 
   return (
