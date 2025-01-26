2ec698b90a6cad1a1353e8802d8534691d9aefff
Jeremy Ward <exochos@gmail.com>
2025-01-26 12:35:39 -0800

    Finished moving everything to const file

--- app/page.tsx
+++ app/page.tsx
@@ -4,105 +4,83 @@ import {
   Card,
   CardActions,
   CardContent,
-  Stack,
   Typography,
+  Container,
 } from "@mui/material";
 import Footer from "../components/Footer";
-import Link from "next/link";
 import Image from "next/image";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
-import { Brightness2 } from "@mui/icons-material";
 import mainConst from "./mainConst.json";
+import Hero from "../components/Hero";
 
 export default function Home() {
+  const { textContent, photoData } = mainConst;
+  const heroImage = "/images/familys-historic-home.jpg";
+
   return (
     <>
-      <Stack>
-        <Box
-          margin="auto"
-          textAlign={"center"}
-          justifyContent={"center"}
-          sx={{ paddingTop: "120px", backgroundColor: "white" }}
-        >
-          <Typography component="h1" variant="h4" sx={{ pb: 4 }}>
-            {mainConst.textContent.title}
-          </Typography>
-          <Typography>
-            {/* old */}
-            {/* <Link href="tel:+19078417274" passHref>
-            <Box component="a" sx={{ color: "inherit", textDecoration: "none", "&:hover": { textDecoration: "underline" } }}>
-              (907) 841-7274
-            </Box>
-          </Link> */}
-
-            {/* updated to */}
-            <Box
-              component="a"
-              href="tel:+19078417274"
-              sx={{
-                color: "inherit",
-                textDecoration: "none",
-                "&:hover": { textDecoration: "underline" },
-              }}
-            >
-              {" "}
-              (907)8417274
-            </Box>
-          </Typography>
-
-          <Typography>
-            {/* old */}
-            {/* <Link href="mailto:ryan@pelletier.construction" passHref>
-            <Box component="a" sx={{ color: "inherit", textDecoration: "none", "&:hover": { textDecoration: "underline" } }}>
-              ryan@pelletier.construction
-            </Box>
-          </Link> */}
-
-            {/* updated to */}
-            <Box
-              component="a"
-              href="mailto: ryan@pelletier.construction"
-              sx={{
-                color: "inherit",
-                textDecoration: "none",
-                "&:hover": { textDecoration: "underline" },
-              }}
-            >
-              {" "}
-              ryan@pelletier.construction
-            </Box>
-          </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            href="/contact"
-            sx={{ mt: 3, fontSize: "1.2rem", width: "200px", height: "50px" }}
+      <Hero imageSrc={heroImage} altText={textContent.title}>
+        <Typography component="h1" variant="h4" sx={{ py: 2 }}>
+          {textContent.title}
+        </Typography>
+        <br />
+        <Typography>
+          <Box
+            component="a"
+            href={textContent.phoneNumber}
+            sx={{
+              color: "inherit",
+              textDecoration: "none",
+              "&:hover": { textDecoration: "underline" },
+            }}
           >
-            Contact Us
-          </Button>
-        </Box>
-      </Stack>
+            {textContent.phoneNumberFormatted}
+          </Box>
+        </Typography>
+        <Typography>
+          <Box
+            component="a"
+            href={`mailto:${textContent.email}`}
+            sx={{
+              color: "inherit",
+              textDecoration: "none",
+              "&:hover": { textDecoration: "underline" },
+            }}
+          >
+            {textContent.email}
+          </Box>
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          href="/contact"
+          sx={{ mt: 3, fontSize: "1.2rem", width: "200px", height: "50px" }}
+        >
+          {textContent.contactButton}
+        </Button>
+      </Hero>
+
       <Typography
         component="h1"
         variant="h4"
         sx={{ pb: 5, textAlign: "center", mt: 3 }}
       >
-        EXQUISITE CRAFTSMANSHIP & CARPENTRY
+        {textContent.craftsmanshipTitle}
       </Typography>
       <ImageList
         sx={{ width: "auto", height: "auto", marginLeft: 5, marginRight: 5 }}
         cols={3}
         rowHeight={330}
       >
-        {itemData.map((item) => (
+        {photoData.map((item) => (
           <ImageListItem
             key={item.img}
             sx={{ minWidth: 248, marginLeft: 1, marginRight: 1 }}
           >
             <Image
-              src={`${item.img}`}
+              src={item.img}
               alt={item.title}
               width={330}
               height={330}
@@ -135,7 +113,7 @@ export default function Home() {
             fontSize="36px"
             sx={{ pb: 2, textAlign: "center", marginTop: 2 }}
           >
-            EXPERIENCE MATTERS.
+            {textContent.experienceTitle}
           </Typography>
           <Typography
             color="white"
@@ -148,11 +126,7 @@ export default function Home() {
               marginRight: "10%",
             }}
           >
-            We specialize in all aspects of home remodeling and improvements.
-            Bathrooms, kitchens, decks, fences, sheds, finishing basements, and
-            most other home improvements or repairs. Our goal is the provide the
-            best customer experience and satisfaction in the industry. Give us a
-            call for a free estimate.
+            {textContent.experienceDescription}
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
@@ -169,7 +143,7 @@ export default function Home() {
               marginRight: "auto",
             }}
           >
-            GET A FREE ESTIMATE
+            {textContent.estimateButton}
           </Button>
         </CardActions>
       </Card>
@@ -177,54 +151,3 @@ export default function Home() {
     </>
   );
 }
-
-const itemData = [
-  {
-    img: "/images/index/open-concept-kitchen.jpg",
-    title: "Open Concept Kitchen",
-  },
-  {
-    img: "/images/index/mukilteo_house.jpg",
-    title: "Mukilteo House",
-  },
-  {
-    img: "/images/index/wrap-around-deck-with-patio-cover.jpg",
-    title: "Wrap around deck with patio cover",
-  },
-  {
-    img: "/images/index/ranch_home_update.jpg",
-    title: "Ranch Home Update",
-  },
-  {
-    img: "/images/index/timber_frame_barn.jpg",
-    title: "Timber Frame Barn",
-  },
-  {
-    img: "/images/index/composite-deck.jpg",
-    title: "Composite Deck",
-  },
-  {
-    img: "/images/index/great_wall_of_lake_city.jpg",
-    title: "Great Wall of Lake City",
-  },
-  {
-    img: "/images/index/bathroom_expansion.jpg",
-    title: "Bathroom Expansion",
-  },
-  {
-    img: "/images/index/railing.jpg",
-    title: "Railing",
-  },
-  {
-    img: "/images/index/bathroom.jpg",
-    title: "Bathroom",
-  },
-  {
-    img: "/images/index/modern-porch.jpg",
-    title: "Modern Porch",
-  },
-  {
-    img: "/images/index/new-shop.jpg",
-    title: "New Shop",
-  },
-];
